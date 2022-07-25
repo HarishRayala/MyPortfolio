f8b76c5262d7081529a86cfb70e176804a932c07
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-07-25 15:02:02 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Harish Rayala</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} AK</h3>
+          <h3>Copyright © {year} HR</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
